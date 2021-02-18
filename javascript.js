4931de88a4e14346b295d49caebe3703e6a74505
blmccavanagh <76139725+blmccavanagh@users.noreply.github.com>
2021-02-19 06:44:50 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,31 +1,27 @@
-<script type="text/javascript">
+document.getElementById("button1").addEventListener("click", function(){
 
-        document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "300px";
+    document.getElementById("box").style.width = "300px";
 
-            document.getElementById("box").style.height = "300px";
-            document.getElementById("box").style.width = "300px";
+});
 
-        });
+document.getElementById("button2").addEventListener("click", function(){
 
-        document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor = "blue";
 
-            document.getElementById("box").style.color = "blue";
+});
 
-        });
+document.getElementById("button3").addEventListener("click", function(){
 
-        document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("box").style.opacity = "0.5";
 
-            document.getElementById("box").style.opacity = "0.5";
+});
 
-        });
+document.getElementById("button4").addEventListener("click", function(){
 
-        document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1.0";
 
-            document.getElementById("box").style.height = "150px";
-            document.getElementById("box").style.width = "150px";
-            document.getElementById("box").style.color = "orange";
-            document.getElementById("box").style.opacity = "1.0";
-
-        });
-
-    </script>
+});
