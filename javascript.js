84f4ef87560731846d1a44e0edd15b97c9d47a95
Bridget McCavanagh <blmccavanagh@gmail.com>
2021-02-19 03:02:33 +0800

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+
+        document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "300px";
+            document.getElementById("box").style.width = "300px";
+
+        });
+
+        document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").style.color = "blue";
+
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").style.opacity = "0.5";
+
+        });
+
+        document.getElementById("button4").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.width = "150px";
+            document.getElementById("box").style.color = "orange";
+            document.getElementById("box").style.opacity = "1.0";
+
+        });
+
+    </script>
